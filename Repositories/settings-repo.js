a5992cf5ce6e28af781a479e82433a7e45ee4c81
psc3245 <psc3.omaha@gmail.com>
2026-03-29 17:04:31 -0500

    repo

--- Repositories/settings-repo.js
+++ Repositories/settings-repo.js
@@ -1,4 +1,3 @@
-import { de } from "zod/locales";
 import pool from "../db/db.js";
 
 export async function findAll() {
@@ -26,4 +25,22 @@ export async function createSettings(id, settings) {
     [id, display_mode, left_default_page, right_default_page, default_habit_type]
   );
   return result.rows[0];
+}
+
+export async function updateSettings(id, updates) {
+    const old = await pool.query('SELECT * FROM settings WHERE user_id = $1', id);
+    const existing = old.rows[0];
+    if (!existing) return null;
+
+  const display_mode = updates.display_mode ?? existing.display_mode;
+  const left_default_page = updates.left_default_page ?? existing.left_default_page;
+  const right_default_page = updates.right_default_page ?? existing.right_default_page;
+  const default_habit_type = updates.default_habit_type ?? existing.default_habit_type;
+
+  const result = await pool.query(
+    'UPDATE settings SET display_mode = $1, left_default_page = $2, right_default_page = $3, default_habit_type = $4 WHERE user_id = $5 RETURNING *',
+    [display_mode, left_default_page, right_default_page, default_habit_type, id]
+  )
+  return result.rows[0] ?? null;
+
 }
