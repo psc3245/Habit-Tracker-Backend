414959f4e90390ac4639cf25ae9ee7f83660bf75
psc3245 <psc3.omaha@gmail.com>
2026-03-29 17:12:29 -0500

    small change, settings is working

--- Services/settings-service.js
+++ Services/settings-service.js
@@ -1,12 +1,25 @@
 import * as z from "zod";
 import * as settingsRepo from "../Repositories/settings-repo.js";
 
-export const settingsSchema = z.object({
-  display_mode: z.enum(["light", "dark"]).optional(),
-  left_default_page: z.enum(["profile", "habits"]).optional(),
-  right_default_page: z.enum(["profile", "glance", "stats"]).optional(),
-  default_habit_type: z.enum(["checkbox", "counter", "duration", "slider"]).optional(),
-});
+export const settingsSchema = z
+  .object({
+    display_mode: z.enum(["light", "dark"]).optional(),
+    left_default_page: z.enum(["profile", "habits"]).optional(),
+    right_default_page: z.enum(["profile", "glance", "stats"]).optional(),
+    default_habit_type: z
+      .enum(["checkbox", "counter", "duration", "slider"])
+      .optional(),
+  })
+  .refine(
+    (data) =>
+      data.display_mode ||
+      data.left_default_page ||
+      data.right_default_page ||
+      data.default_habit_type,
+    {
+      message: "Must provide at least one field to update",
+    },
+  );
 
 export async function getSettingsForUserId(userId) {
   if (!userId) throw new Error("userId required");
